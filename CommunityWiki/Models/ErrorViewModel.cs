677f8dcc780618d2ef24ef911f23e0cbe67cadc2
Matt Millican <matt@mattmillican.com>
2017-09-10 17:17:23 -0500

    Add project files.

---
+++ CommunityWiki/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CommunityWiki.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
