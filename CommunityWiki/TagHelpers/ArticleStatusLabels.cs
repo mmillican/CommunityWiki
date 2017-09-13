77f50ed3ac2b487b6bd46a251eab1d2721a783f8
Matt Millican <matt@mattmillican.com>
2017-09-12 23:03:41 -0500

    Create tag helper for article status labels

---
+++ CommunityWiki/TagHelpers/ArticleStatusLabels.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using CommunityWiki.Models.Articles;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CommunityWiki.TagHelpers
+{
+    /// <summary>
+    /// Displays labels based on the article's status
+    /// </summary>
+    [HtmlTargetElement("article-status", Attributes = nameof(Article), TagStructure = TagStructure.WithoutEndTag)]
+    public class ArticleStatusLabels : TagHelper
+    {
+        public ArticleModel Article { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var sb = new StringBuilder();
+
+            if (!Article.PublishedOn.HasValue)
+                sb.AppendLine("<span class=\"label label-default\" title=\"This article is not published\">Not published</span>");
+
+            if (Article.IsFlaggedForReview)
+                sb.AppendLine("<span class=\"label label-warning\" title=\"This article is not published\">Needs review</span>");
+
+            if (Article.IsFlaggedForDeletion)
+                sb.Append("<span class=\"label label-danger\" title=\"This article is flagged for review\">Deletion</span>");
+
+            output.Content.SetHtmlContent(sb.ToString());
+
+            output.TagName = "";
+
+        }
+    }
+}
