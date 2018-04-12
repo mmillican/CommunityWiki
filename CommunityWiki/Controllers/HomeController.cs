f33a1ae9b2a2cddd576966db2ce314f844ce82ac
Matt Millican <matt@mattmillican.com>
2018-04-11 22:44:03 -0500

    Implement home page of top/new content

--- CommunityWiki/Controllers/HomeController.cs
+++ CommunityWiki/Controllers/HomeController.cs
@@ -5,14 +5,58 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CommunityWiki.Models;
+using CommunityWiki.Data;
+using CommunityWiki.Models.Home;
+using Microsoft.EntityFrameworkCore;
+using CommunityWiki.Models.Articles;
+using AutoMapper.QueryableExtensions;
 
 namespace CommunityWiki.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _dbContext;
+
+        public HomeController(ApplicationDbContext dbContext)
         {
-            return View();
+            _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = new HomeViewModel();
+
+            var topTypes = _dbContext.Articles
+                .Where(x => x.PublishedOn.HasValue
+                    && !x.DeletedOn.HasValue)
+                .GroupBy(x => x.ArticleType)
+                .OrderByDescending(x => x.Count())
+                .Take(5)
+                .Select(x => new TopArticleTypeModel
+                {
+                    ArticleType = x.Key.ToModel(),
+                    Count = x.Count()
+                });
+
+            var recentlyCreatedArticles = _dbContext.Articles
+                .Where(x => x.PublishedOn.HasValue
+                    && !x.DeletedOn.HasValue)
+                .OrderByDescending(x => x.CreatedOn)
+                .Take(10)
+                .ProjectTo<ArticleModel>();
+
+            var recentlyUpdatedArticles = _dbContext.Articles
+                .Where(x => x.PublishedOn.HasValue
+                    && !x.DeletedOn.HasValue)
+                .OrderByDescending(x => x.UpdatedOn)
+                .Take(10)
+                .ProjectTo<ArticleModel>();
+
+            model.TopTypes = await topTypes.ToListAsync();
+            model.RecentlyCreated = await recentlyCreatedArticles.ToListAsync();
+            model.RecentlyUpdated = await recentlyUpdatedArticles.ToListAsync();
+
+            return View(model);
         }
 
         public IActionResult Error()
---
+++ CommunityWiki/Models/Home/HomeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommunityWiki.Models.Articles;
+using CommunityWiki.Models.ArticleTypes;
+
+namespace CommunityWiki.Models.Home
+{
+    public class HomeViewModel
+    {
+        public List<ArticleModel> RecentlyCreated { get; set; } = new List<ArticleModel>();
+        public List<ArticleModel> RecentlyUpdated { get; set; } = new List<ArticleModel>();
+
+        public List<TopArticleTypeModel> TopTypes { get; set; } = new List<TopArticleTypeModel>();
+    }
+
+    public class TopArticleTypeModel
+    {
+        public ArticleTypeModel ArticleType { get; set; }
+
+        public int Count { get; set; }
+    }
+}
