c193bb766d2f76206fa0bfe9809296f05da5cf6a
Matt Millican <matt@mattmillican.com>
2019-01-21 22:30:03 -0600

    Main navigation

---
+++ CommunityWiki/Components/MainNavigation.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using CommunityWiki.Data;
+using CommunityWiki.Models.ArticleTypes;
+using CommunityWiki.Models.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunityWiki.Components
+{
+    public class MainNavigation : ViewComponent
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public MainNavigation(ApplicationDbContext dbContext,
+            IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = new MainNavigationViewModel();
+            model.ArticleTypes = await _dbContext.ArticleTypes
+                .OrderBy(x => x.Name)
+                .ProjectTo<ArticleTypeModel>(_mapper.ConfigurationProvider)
+                .ToListAsync();
+
+            return View(model);
+        }
+    }
+}
---
+++ CommunityWiki/Models/Components/MainNavigationViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityWiki.Models.ArticleTypes;
+using System.Collections.Generic;
+
+namespace CommunityWiki.Models.Components
+{
+    public class MainNavigationViewModel
+    {
+        public List<ArticleTypeModel> ArticleTypes { get; set; }
+    }
+}
